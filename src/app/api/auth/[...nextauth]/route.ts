9878c5d6415cd58d309fc7a31f11ebbeef5ddbdd
Bruna Oliveira <79415713+bruna-luc@users.noreply.github.com>
2023-07-09 10:05:17 -0300

    feat: google autentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,11 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import { Adapter } from "next-auth/adapters";
 
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/primas";
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
@@ -14,4 +13,7 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-});
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
